81ac0bbd8e59db1e1ab71d0a3dc49b626263a1bb
Andrew Nadezhin <an.nadezhin@gmail.com>
2017-11-29 15:31:00 +0300

    new rep

---
+++ differentiator.cpp
@@ -0,0 +1,325 @@
+#include <iostream>
+#include "differentiator.h"
+#include <cstring>
+#include <cassert>
+
+void texmaker();
+
+int main() {
+    int pos = 0;
+    char *buf = "(* (sin x) (sin y))";
+    Expression *node_1 = create_tree(buf, &pos);
+    Expression *node_2 = node_1->derivative("x");
+    Expression *node_3 = node_2->optimize();
+    print_JPEG(node_3);
+    texmaker();
+}
+
+void texmaker() {
+    FILE* texf = fopen("expression.tex", "w");
+    fprintf(texf, "\\documentclass{article}\n"
+            "\\begin{document}\n"
+            "    Hello, world!\n i am andrew \n"
+            "\\end{document}");
+    fclose(texf);
+    system("pdflatex expression.tex");
+ //   system("");
+}
+
+
+Expression *Operator_bin::optimize() {
+    Expression *x = arg_f->optimize();
+    Expression *y = arg_s->optimize();
+
+    switch (type_bin_op) {
+        case MUL:
+            if (Number *x_0 = dynamic_cast<Number *>(x)) {
+                if (x_0->num() == 0) return x_0;
+                if (x_0->num() == 1) return y;
+            }
+            if (Number *y_0 = dynamic_cast<Number *>(y)) {
+                if (y_0->num() == 0) return y_0;
+                if (y_0->num() == 1) return y;
+            }
+            break;
+        case ADD:
+            if (Number *x_0 = dynamic_cast<Number *>(x)) {
+                if (x_0->num() == 0) return y;
+            }
+            if (Number *y_0 = dynamic_cast<Number *>(y)) { ////class num()
+                if (y_0->num() == 0) return x;
+            }
+            break;
+    }
+    return new Operator_bin(type_bin_op, x, y);
+}
+
+Expression *Operator_un::optimize() {
+    Expression *x = arg_f->optimize();
+    return new Operator_un(type_un_op, x);
+}
+
+double Number::num() {
+    return num_val;
+}
+
+
+Expression *Expression::optimize() {
+    return this;
+}
+
+Expression *Expression::derivative(char *x) {
+    assert(0);
+}
+
+Expression *Number::derivative(char *number) {
+    return new Number(0);
+}
+
+Expression *Variable::derivative(char *var) {
+    if (!strcmp(var_name, var)) return new Number(1);
+    return new Number(0);
+}
+
+Expression *Operator_un::derivative(char *var) {
+    switch (type_un_op) {
+        case UN_MINUS:
+            return new Operator_un(UN_MINUS, arg_f->derivative(var));
+        case SIN:
+            return new Operator_bin(MUL,
+                                    arg_f->derivative(var),
+                                    new Operator_un(COS, arg_f));
+        case COS:
+            return new Operator_bin(MUL,
+                                    new Operator_un(UN_MINUS, arg_f->derivative(var)),
+                                    new Operator_un(SIN, arg_f));
+        case SQRT:
+            return new Operator_bin(DIV,
+                                    arg_f->derivative(var),
+                                    new Operator_bin(MUL, new Number(2), this));
+    }
+}
+
+
+Expression *Operator_bin::derivative(char *var) {
+    switch (type_bin_op) {
+        case ADD:
+            return new Operator_bin(ADD,
+                                    arg_f->derivative(var),
+                                    arg_s->derivative(var));
+        case SUB:
+            return new Operator_bin(SUB,
+                                    arg_f->derivative(var),
+                                    arg_s->derivative(var));
+        case MUL:
+            return new Operator_bin(ADD,
+                                    new Operator_bin(MUL, arg_f->derivative(var), arg_s),
+                                    new Operator_bin(MUL, arg_s->derivative(var), arg_f));
+        case DIV:
+            return new Operator_bin(SUB,
+                                    new Operator_bin(DIV, arg_f->derivative(var), arg_s),
+                                    new Operator_bin(MUL, this, new Operator_bin(DIV, arg_s->derivative(var), arg_s)));
+    }
+
+}
+
+Expression *create_tree(char *buffer, int *position) {
+
+    skip_s(buffer, position);
+
+    if (buffer[*position] == '(') {
+        int counter = 0;
+        char value[BUF_LENGTH] = {0};
+        (*position)++;
+        while (!isspace(buffer[*position])) {
+            value[counter++] = buffer[(*position)++];
+        }
+        int bin_act = compare_str_bin(value);
+        if (bin_act != ERROR) {
+            skip_s(buffer, position);
+            Expression *new_node_l = create_tree(buffer, position);
+            skip_s(buffer, position);
+            Expression *new_node_r = create_tree(buffer, position);
+            skip_s(buffer, position);
+            assert(buffer[*position] == ')');
+            (*position)++;
+            return new Operator_bin(bin_act, new_node_l, new_node_r);
+        }
+
+        int un_act = compare_str_un(value);
+        if (un_act != ERROR) {
+            skip_s(buffer, position);
+            Expression *new_node_l = create_tree(buffer, position);
+            skip_s(buffer, position);
+            assert(buffer[*position] == ')');
+            (*position)++;
+            return new Operator_un(un_act, new_node_l);
+        }
+        assert(0);
+    }
+
+    int counter = 0;
+    char value[BUF_LENGTH] = {0};
+    while (!isspace(buffer[*position]) && buffer[*position] != ')')
+        value[counter++] = buffer[(*position)++];
+
+    if (isalpha(value[0])) {
+        return new Variable(value);
+    }
+
+    if (isdigit(value[0])) {
+        return new Number(atof(value));
+    }
+    assert(0);
+}
+
+
+int compare_str_bin(char *value) {
+
+    if (!strcmp(value, "+")) return ADD;
+    if (!strcmp(value, "-")) return SUB;
+    if (!strcmp(value, "*")) return MUL;
+    if (!strcmp(value, "/")) return DIV;
+    if (!strcmp(value, "*")) return MUL;
+    return ERROR;
+}
+
+int compare_str_un(char *value) {
+
+    if (!strcmp(value, "sin")) return SIN;
+    if (!strcmp(value, "cos")) return COS;
+    if (!strcmp(value, "sqrt")) return SQRT;
+    if (!strcmp(value, "neg")) return UN_MINUS;
+    return ERROR;
+}
+
+
+void skip_s(char *buffer, int *position) {
+    while (isspace(buffer[*position]))
+        (*position)++;
+}
+
+Expression::Expression() {
+}
+
+
+void Expression::print_dot_name(FILE *code) {
+}
+
+void Operator_un::print_dot(FILE *code) {
+    fprintf(code, "\"");
+    print_dot_name(code);
+    fprintf(code, "\"->\"");
+    arg_f->print_dot_name(code);
+    fprintf(code, "\"\n");
+    arg_f->print_dot(code);
+}
+
+void Operator_bin::print_dot(FILE *code) {
+    fprintf(code, "\"");
+    print_dot_name(code);
+    fprintf(code, "\"->\"");
+    arg_f->print_dot_name(code);
+    fprintf(code, "\"\n");
+    arg_f->print_dot(code);
+
+    fprintf(code, "\"");
+    print_dot_name(code);
+    fprintf(code, "\"->\"");
+    arg_s->print_dot_name(code);
+    fprintf(code, "\"\n");
+    arg_s->print_dot(code);
+}
+
+void Expression::print_dot(FILE *code) {
+}
+
+Variable::Variable(char *val) {
+    var_name = strdup(val);
+}
+
+Variable::~Variable() {
+    free(var_name);
+}
+
+Number::Number(double val) {
+    num_val = val;
+}
+
+Operator_un::Operator_un(int val, Expression *arg) {
+    assert(val >= SIN && val <= UN_MINUS);
+    assert(arg != nullptr);
+    type_un_op = val;
+    arg_f = arg;
+}
+
+Operator_bin::Operator_bin(int val, Expression *arg_l, Expression *arg_r) {
+    assert(val >= ADD && val <= DIV);
+    assert(arg_l != nullptr && arg_r != nullptr);
+    type_bin_op = val;
+    arg_f = arg_l;
+    arg_s = arg_r;
+}
+
+void print_JPEG(Expression *diff) {
+    FILE *code = fopen("diff.dot", "w");
+    fprintf(code, "digraph G{\n");
+    diff->print_dot(code);
+    fprintf(code, "}");
+    fclose(code);
+    system("dot -Tjpeg diff.dot -o diff.jpeg");
+}
+
+
+void Operator_un::print_dot_name(FILE *code) {
+    fprintf(code, "(");
+    switch (type_un_op) {
+        case SIN :
+            fprintf(code, "sin");
+            break;
+        case COS :
+            fprintf(code, "cos");
+            break;
+        case SQRT :
+            fprintf(code, "sqrt");
+            break;
+        case UN_MINUS :
+            fprintf(code, "minus");
+            break;
+    }
+    fprintf(code, " ");
+    arg_f->print_dot_name(code);
+    fprintf(code, ")");
+}
+
+
+void Variable::print_dot_name(FILE *code) {
+    fprintf(code, "%s", var_name);
+}
+
+void Operator_bin::print_dot_name(FILE *code) {
+    fprintf(code, "(");
+    switch (type_bin_op) {
+        case ADD :
+            fprintf(code, "+");
+            break;
+        case SUB :
+            fprintf(code, "-");
+            break;
+        case MUL:
+            fprintf(code, "*");
+            break;
+        case DIV :
+            fprintf(code, "/");
+            break;
+    }
+    fprintf(code, " ");
+    arg_f->print_dot_name(code);
+    fprintf(code, " ");
+    arg_s->print_dot_name(code);
+    fprintf(code, ")");
+}
+
+void Number::print_dot_name(FILE *code) {
+    fprintf(code, "%g", num_val);
+}
---
+++ differentiator.h
@@ -0,0 +1,86 @@
+//
+// Created by andrew on 22.11.17.
+//
+
+#ifndef DIFFERENTIATOR_DIFFERENTIATOR_H
+#define DIFFERENTIATOR_DIFFERENTIATOR_H
+
+const int BUF_LENGTH = 50;
+
+const int ERROR = -1;
+
+enum UnOp {
+    SIN, COS, SQRT, UN_MINUS
+};
+
+enum BinOp {
+    ADD, MUL, SUB, DIV
+};
+
+class Expression {
+public:
+    Expression();
+    virtual void print_dot(FILE *code);
+    virtual void print_dot_name(FILE *code);
+    virtual Expression *derivative(char *);
+    virtual Expression *optimize();
+};
+
+class Variable : public Expression {
+public:
+    Variable(char *);
+    void print_dot_name(FILE *code);
+    ~Variable();
+    Expression * derivative(char*);
+private:
+    char *var_name;
+};
+
+class Number : public Expression {
+public:
+    Number(double);
+    void print_dot_name(FILE *code);
+    Expression * derivative(char*);
+    double num();
+
+private:
+    double num_val;
+};
+
+class Operator_un : public Expression {
+public:
+    Operator_un(int, Expression *);
+    void print_dot(FILE *code);
+    void print_dot_name(FILE *code);
+    Expression * derivative(char*);
+    Expression *optimize();
+private:
+    int type_un_op;
+    Expression *arg_f;
+};
+
+class Operator_bin : public Expression {
+public:
+    Operator_bin(int, Expression *, Expression *);
+    void print_dot(FILE* code);
+    void print_dot_name(FILE *code);
+    Expression *derivative(char *);
+    Expression *optimize();
+private:
+    int type_bin_op;
+    Expression *arg_f;
+    Expression *arg_s;
+};
+
+void print_JPEG(Expression *diff);
+
+void skip_s(char *buffer, int *position);
+
+int compare_str_bin(char *value);
+
+int compare_str_un(char *value);
+
+Expression *create_tree(char *buffer, int *position);
+
+
+#endif //DIFFERENTIATOR_DIFFERENTIATOR_H
